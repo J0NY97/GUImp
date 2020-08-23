36a22c6503b56596d1b4a7d4f51e3057000500c5
Epicurius <nneronin@student.hive.fi>
2020-08-23 14:35:23 +0300

    made it better

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 19:15:07 by nneronin          #+#    #+#             */
-/*   Updated: 2020/08/20 19:15:41 by nneronin         ###   ########.fr       */
+/*   Updated: 2020/08/23 13:40:59 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,29 @@
 
 void	draw(SDL_Event event, t_element *elem)
 {
-	t_line l;
+	t_shapes l;
 	t_brush *brush;
-	t_circle c;
 	int x;
 	int y;
 
 	brush = (t_brush *)elem->info;
 	x = event.button.x - elem->x;
 	y = event.button.y - elem->y;
-	c.r = brush->size;
-	c.xc = x;
-	c.yc = y;
+	l.size = brush->size;
 	if (event.type == SDL_MOUSEBUTTONUP)
 		brush->draw = 0;
 	else if (event.type == SDL_MOUSEBUTTONDOWN)
 		brush->draw = 1;
 	if (brush->draw == 1)
 	{
-		if (brush->old_x == -1 && brush->old_y == -1)
-			ft_create_circle(elem->surface, brush->color, c, 1);
-		l.y2 = y;
 		l.x2 = x;
+		l.y2 = y;
 		l.x1 = brush->old_x;
 		l.y1 = brush->old_y;
+		if (brush->old_x == -1 && brush->old_y == -1)
+			ft_create_circle(elem->surface, brush->color, l, 1);
 		if (brush->old_x != -1 && brush->old_y != -1)
-			ft_create_line(elem->surface, brush->color, brush->size, &l);
+			ft_create_line(elem->surface, brush->color, l);
 		brush->old_x = x;
 		brush->old_y = y;
 	}
