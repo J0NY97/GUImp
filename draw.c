cec9e22b4b1f212b1ae48d81bc1b55e90c44007c
Jony Salmi <jsalmi@c1r6p18.hive.fi>
2020-08-25 15:16:00 +0300

    fixed floodfill

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 19:15:07 by nneronin          #+#    #+#             */
-/*   Updated: 2020/08/23 15:21:31 by jsalmi           ###   ########.fr       */
+/*   Updated: 2020/08/25 11:00:21 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	draw(SDL_Event event, t_element *elem)
 			if (brush->type == 1)
 				ft_create_circle(elem->surface, brush->color, l, 1);
 			if (brush->type == 2)
-				// click click for rectangle
+			{} // click click for rectangle
 		}
 		if (brush->old_x != -1 && brush->old_y != -1)
 			ft_create_line(elem->surface, brush->color, l);
