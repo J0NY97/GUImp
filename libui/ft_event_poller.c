ee02c70632190f50f7798031d98cbac745f95916
J0NY97 <jony.salmi97@gmail.com>
2020-09-03 13:01:08 +0300

    added while loop to ft_event_poller

--- libui/ft_event_poller.c
+++ libui/ft_event_poller.c
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/19 15:16:58 by jsalmi            #+#    #+#             */
-/*   Updated: 2020/09/03 12:32:12 by nneronin         ###   ########.fr       */
+/*   Updated: 2020/09/03 12:59:56 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	ft_event_poller(t_libui *libui)
 {
 	t_list *win;
 
-	SDL_PollEvent(&libui->event);
+	while(SDL_PollEvent(&libui->event))
 	{
 		if (libui->event.type == SDL_QUIT)
 			exit (1);
