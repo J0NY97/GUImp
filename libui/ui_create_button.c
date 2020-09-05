432d620b8e602cc28f486caf78a1f019ab29ef2d
J0NY97 <jony.salmi97@gmail.com>
2020-09-05 11:07:31 +0300

    made a function that will set the default options for an element so we will spara more lines in the element creation functions

--- libui/ui_create_button.c
+++ libui/ui_create_button.c
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 11:04:46 by jsalmi            #+#    #+#             */
-/*   Updated: 2020/09/03 16:54:38 by jsalmi           ###   ########.fr       */
+/*   Updated: 2020/09/05 11:01:02 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,17 @@ t_element	*ui_create_button(t_window *win, t_xywh coord)
 	t_element_info info;
 	t_element	*elem;
 
-	info.coord = coord;
+/*
+ 	info.coord = coord;
 	info.parent = win->surface; // think about this
-	info.info = (t_button *)malloc(sizeof(t_button));
-	info.info_size = 0; // remove this in another iteration
 	info.f = &default_click;
 	info.event_handler = &ft_event_handler;
 	info.bg_color = 0xf5f5f5;
 	info.extra_info = NULL;
+*/
+	info = ft_default_elem_info(win, coord);
+	info.info = (t_button *)malloc(sizeof(t_button));
+	info.info_size = 0; // remove this in another iteration
 	info.set_text = 1;
 	// @Improvement: move this to set t_text or something liek that
 	{
