a06cce860c89329a28d04d52249d773b73562c24
Epicurius <nneronin@student.hive.fi>
2020-09-05 11:11:01 +0300

    popup leaks only a little

--- libui/popup.c
+++ libui/popup.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/29 14:01:15 by nneronin          #+#    #+#             */
-/*   Updated: 2020/09/03 13:23:40 by nneronin         ###   ########.fr       */
+/*   Updated: 2020/09/03 16:35:44 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,10 @@ char		*input_popup(int x1, int y1)
 	free(win);
 	char *str;
 	str = ft_strdup(buttons[1]->text.text);
+	free(win);
+	free(buttons[0]);
+	free(buttons[1]);
+	free(libui);
 	return (str);
 }
 
@@ -87,7 +91,7 @@ int			true_false_popup(int x1, int y1, char *msg)
 {
 	t_window		*win;
 	t_window_info	test;
-	t_libui *libui;
+	t_libui			*libui;
 	t_element *buttons[2];
 
 	libui = (t_libui *)malloc(sizeof(t_libui));
@@ -96,7 +100,6 @@ int			true_false_popup(int x1, int y1, char *msg)
 	test.title = ft_strdup("Pop_up");
 	win = ft_create_window(libui, test);
 	ft_update_background(win->surface, 0xECECEC);
-
 	int result;
 	t_xywh coord;
 
@@ -135,11 +138,21 @@ int			true_false_popup(int x1, int y1, char *msg)
 	text.parent = win->surface;
 	text.font = TTF_OpenFont("./libui/TTF/OpenSans.ttf", 22);
 	ft_create_text(&text);
+	free(text.text);
+	TTF_CloseFont(text.font);
+	text.font = NULL;
 
 	ui_render(win);
 	while (result == -1)
 		ft_event_poller(libui);
-	SDL_DestroyWindow(win->win);
-	free(win);
-	return (result);
+
+
+	free_element(buttons[1]);
+	free_element(buttons[0]);
+	free_window(win);
+
+	free(libui->windows);
+	free(libui->hotkeys);
+	free(libui);
+	return (0);
 }
