59cd0f6e21a14111b1c4847342b3b5953db6f23a
J0NY97 <jony.salmi97@gmail.com>
2020-09-12 13:57:19 +0300

    removes the text automatically from element when putting image on it

--- libui/ft_set_element_image.c
+++ libui/ft_set_element_image.c
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 12:30:44 by jsalmi            #+#    #+#             */
-/*   Updated: 2020/09/12 12:35:59 by jsalmi           ###   ########.fr       */
+/*   Updated: 2020/09/12 13:56:43 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ft_set_element_image(t_element *elem, SDL_Surface *img)
 {
 	SDL_Rect temp;
 
+	elem->set_text = 0;
 	temp.h = elem->surface->h;
 	temp.w = temp.h;
 	temp.x = temp.w / 2;
