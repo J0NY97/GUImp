75c7efa9c25c21d285d4bed38147dc8acd8141b1
Epicurius <nneronin@student.hive.fi>
2020-09-17 16:48:25 +0300

    remove ft_strndup from file

--- libui/input_popup.c
+++ libui/input_popup.c
@@ -6,28 +6,11 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/29 14:01:15 by nneronin          #+#    #+#             */
-/*   Updated: 2020/09/17 16:43:00 by nneronin         ###   ########.fr       */
+/*   Updated: 2020/09/17 16:46:36 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libui.h"
-
-char	*ft_strndup(const char *s1, ssize_t len)
-{
-	ssize_t	counter;
-	char	*sdest;
-
-	counter = 0;
-	if (!(sdest = (char *)ft_memalloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	while (s1[counter] != '\0' && counter < len)
-	{
-		sdest[counter] = s1[counter];
-		counter++;
-	}
-	sdest[counter] = '\0';
-	return (sdest);
-}
 
 void		text_area(SDL_Event e, t_element *elem)
 {
