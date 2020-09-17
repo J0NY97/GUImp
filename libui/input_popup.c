0b57f4d6636eb852a3e9a543b091daa7055e6ba2
J0NY97 <jony.salmi97@gmail.com>
2020-09-17 16:51:49 +0300

    not sure

--- libui/input_popup.c
+++ libui/input_popup.c
@@ -6,28 +6,11 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/29 14:01:15 by nneronin          #+#    #+#             */
-/*   Updated: 2020/09/17 13:16:55 by jsalmi           ###   ########.fr       */
+/*   Updated: 2020/09/17 15:48:03 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libui.h"
-
-char	*ft_strndup(const char *s1, ssize_t len)
-{
-	ssize_t	counter;
-	char	*sdest;
-
-	counter = 0;
-	if (!(sdest = (char *)ft_memalloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	while (s1[counter] != '\0' && counter < len)
-	{
-		sdest[counter] = s1[counter];
-		counter++;
-	}
-	sdest[counter] = '\0';
-	return (sdest);
-}
 
 static void		button(SDL_Event e, t_element *elem)
 {
