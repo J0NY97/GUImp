a2a6a88fe658a0e5b03cc3e72108ac3cd70547c0
J0NY97 <jony.salmi97@gmail.com>
2020-09-17 13:35:59 +0300

    fixed niklas leaks

--- libui/ft_default_text.c
+++ libui/ft_default_text.c
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 11:18:45 by jsalmi            #+#    #+#             */
-/*   Updated: 2020/09/17 13:22:35 by jsalmi           ###   ########.fr       */
+/*   Updated: 2020/09/17 13:33:34 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ void	ft_default_text(t_text *text, char *txt)
 		text->set_text = 1;	
 		text->text = ft_strdup(txt);
 	}
-	if (txt != NULL)
-		text->text = ft_strdup(txt);
 	else
 		text->text = NULL;
 	text->color = 0x000000;
